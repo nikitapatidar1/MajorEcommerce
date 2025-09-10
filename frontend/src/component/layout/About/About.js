64f06c639308029895afe4afebe68b05cfaab059
Nikita Patidar <patidarnikita008@gmail.com>
2025-09-10 16:31:51 +0530

    Fix: Added missing profile image

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -65,7 +65,7 @@ import "./aboutSection.css";
 import { Typography, Avatar } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import profilePic from "../../../images/profile.jpg";
+// import profilePic from "../../../images/profile.jpg";
 
 const About = () => {
   return (
@@ -87,7 +87,7 @@ const About = () => {
                 margin: "2vmax 0",
                 border: "2px solid #3f51b5",
               }}
-              src={profilePic}
+              // src={profilePic}
               alt="Founder"
             />
 
